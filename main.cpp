20acbc2e15182313147d1c58dc98e0474c720237
NomAyuG19022 <hj-g19022@sist.ac.jp>
2020-09-07 14:25:25 +0900

    ウィンドウサイズを変更、ウィンドウのタイトル変更

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 //########## ヘッダーファイル読み込み ##########
 #include "DxLib.h"
 
-#define GAME_WIDTH			800	//画面の横の大きさ
-#define GAME_HEIGHT			600	//画面の縦の大きさ
+#define GAME_WIDTH			640	//画面の横の大きさ
+#define GAME_HEIGHT			480	//画面の縦の大きさ
 #define GAME_COLOR			32	//画面のカラービット
 
 #define GAME_WINDOW_BAR		0	//タイトルバーはデフォルトにする
-#define GAME_WINDOW_NAME	"GAME TITLE"	//ウィンドウのタイトル
+#define GAME_WINDOW_NAME	"nekoTV"	//ウィンドウのタイトル
 
 //▼▼▼▼▼ プログラム追加ここから ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
 
